555aef9ae19b2f4936300f4c775910d38c97f51a
Dylan Hawley <dylanthomashawley@gmail.com>
2018-10-21 07:49:52 -0400

    Final

--- obd_hud.py
+++ obd_hud.py
@@ -1,27 +1,11 @@
 from Tkinter import *
 import ttk
-import matplotlib as mpl
-import numpy as np
-import sys
-import matplotlib.backends.tkagg as tkagg
-from matplotlib.backends.backend_agg import FigureCanvasAgg
+# import matplotlib
+# matplotlib.use("TkAgg")
+# from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2TkAgg
+# from matplotlib.figure import Figure
+# import matplotlib.pyplot
 
-def draw_figure(canvas, figure, loc=(0, 0)):
-	figure_canvas_agg = FigureCanvasAgg(figure)
-	figure_canvas_agg.draw()
-	figure_x, figure_y, figure_w, figure_h = figure.bbox.bounds
-	figure_w, figure_h = int(figure_w), int(figure_h)
-	photo = PhotoImage(master=canvas, width=figure_w, height=figure_h)
-
-	# Position: convert from top-left anchor to center anchor
-	canvas.create_image(loc[0] + figure_w/2, loc[1] + figure_h/2, image=photo)
-
-	# Unfortunately, there's no accessor for the pointer to the native renderer
-	tkagg.blit(photo, figure_canvas_agg.get_renderer()._renderer, colormode=2)
-
-	# Return a handle which contains a reference to the photo object
-	# which must be kept live or else the picture disappears
-	return photo
 
 class OBDCluster:
 
@@ -78,26 +62,24 @@ class OBDCluster:
 		self.titleLabel = ttk.Label(topmidframe, text="Fuel Economy", font="Arial 40", background="#d0d0d4")
 		self.titleLabel.pack(side=TOP)
 
-		self.canvas = Canvas(bottommidframe, width=430, height=400)
-		self.canvas.pack()
+		# self.canvas = Canvas(bottommidframe, width=430, height=400)
+		# self.canvas.pack()
 
-		X = np.linspace(0, 2 * np.pi, 50)
-		Y = np.sin(X)
+		# f = pyplot(figsize=(5,5), dpi=100)
+		# f.plot([1,2,3],[1,3,5])
+		# f.xlabel("sp")
+		# f.ylabel("mpg")
+        # a = f.add_subplot(111)
+        # a.plot([1,2,3,4,5,6,7,8],[5,6,1,3,8,9,3,5])
 
-		# Create the figure we desire to add to an existing canvas
-		fig = mpl.figure.Figure(figsize=(2, 1))
-		ax = fig.add_axes([0, 0, 1, 1])
-		ax.plot(X, Y)
+        # self.canvas = FigureCanvasTkAgg(f, bottommidframe)
+        # self.canvas.show()
+        # self.canvas.get_tk_widget().pack(side=BOTTOM, fill=BOTH, expand=True)
 
-		# Keep this handle alive, or else figure will disappear
-		fig_x, fig_y = 100, 100
-		fig_photo = draw_figure(self.canvas, fig, loc=(fig_x, fig_y))
-		fig_w, fig_h = fig_photo.width(), fig_photo.height()
-
-		# Add more elements to the canvas, potentially on top of the figure
-		self.canvas.create_line(200, 50, fig_x + fig_w / 2, fig_y + fig_h / 2)
-		self.canvas.create_text(200, 50, text="Zero-crossing", anchor="s")
-
+        # toolbar = NavigationToolbar2TkAgg(canvas, self)
+        # toolbar.update()
+        # self.canvas._tkcanvas.pack(side=TOP, fill=BOTH, expand=True)
+		
 		def recordData():
 			print("button clicked..")
 
@@ -113,17 +95,8 @@ class OBDCluster:
 		bottomrightframe.pack(side=BOTTOM, padx=.04*screen_width, pady=.1*screen_height)
 		bottomrightframe.configure(background="#dee0e4")
 
-		# w = Canvas(toprightframe, width = screen_width, height=20, bg="#dee0e4")
-		# w.pack(expand=YES, fill=X)
-
-		# root.update()
-		# w.create_line(10, 150, w.winfo_width()-10, 150, fill="#7a7a7a", width=3)
-
 		self.message = Message(toprightframe, text="Capture fuel consuption data at a constant speed", font="Arial 13", background="#dee0e4", width="300")
-		self.message.pack(side=TOP, padx=5, pady=35)
-
-		# style = ttk.Style()
-		# style.configure("S.TButton", foreground="#6e6e6e", background="white")
+		self.message.pack(side=TOP, padx=5, pady=25)
 		
 		self.recordButton = ttk.Button(toprightframe, text="Cruise", command=recordData())
 		self.recordButton.pack(side=BOTTOM)
