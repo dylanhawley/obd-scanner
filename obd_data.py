7c010d964f05d202efd9f0f73277396cc3447aff
Dylan Hawley <dylanthomashawley@gmail.com>
2018-10-25 00:28:56 -0400

    Working v1

---
+++ obd_data.py
@@ -0,0 +1,98 @@
+import obd
+import time
+import csv
+
+
+class SpeedandEfficiency():
+    def _init_(self, speed, efficiency):
+        self.speed = speed
+        self.efficiency = efficiency
+SEList = []
+maxSE = SpeedandEfficiency(0.0,0.0)
+num_loaded = 0
+num_new = 0
+#Records data for speed efficiency calculations
+def EfficiencyButtonRecord():
+    valueRecorded = False
+    fuelRateTotal = 0
+    speedTotal = 0
+    fuelRateCounter = 0
+    speedCounter = 0
+    minSpeed = 0
+    maxSpeed = 0
+    def new_spd(obdqueryrefer) :
+        speedquery = obdqueryrefer.value.magnitude
+        if speedQuery < minSpeed : minSpeed = speedQuery
+        if speedQuery > maxSpeed : maxSpeed = speedQuery
+        speedTotal += speedquery
+        speedCounter += 1
+    def new_fr(obdqueryrefer) :
+        fuelratequery = obdqueryrefer.value.magnitude
+        fuelRateTotal += fuelratequery
+        fuelRateCounter += 1
+    connectionfr = obd.Async()
+    connections = obd.Async()
+    connectionfr.watch(obd.commands.FUEL_RATE, callback=new_fr)
+    connections.watch(obd.commands.SPEED, callback=new_spd)
+    connectionfr.start()
+    connections.start()
+    t_end = time.time() + 10
+    while time.time() < t_end:
+        pass
+    connectionfr.stop()
+    connections.stop()
+    speedAverage = (SpeedTotal/speedCounter)
+    averageConsumption = (fuelRateTotal/fuelRateCounter)/speedAverage #liters per mile
+    if ((maxSpeed - minSpeed) < 3) :
+        addValue = SpeedandEfficiency(speedAverage, averageConsumption)
+        SEList.append(addValue)
+        valueRecorded = True
+    return valueRecorded
+#keep track of num_loaded for exit func
+def StartupSEList(num_loaded_):
+    with open("fuel_economy.csv", newline='') as csvfile:
+        filereader = csv.reader(csvfile, delimiter = ',')
+        line_count = 0
+        for row in filereader :
+            if(line_count == 0) :
+                line_count += 1
+            elif(line_count == 1) :
+                maxSE.speed = float(row[0])
+                maxSE.efficiency = float(row[1])
+                line_count += 1
+            else :
+                SEList.append(SpeedandEfficiency(float(row[0]),float(row[1])))
+                num_loaded_ += 1
+                line_count += 1
+    return num_loaded_
+def ExitSEList(num_new_):
+    with open("fuel_economy.csv", 'a', newline='') as csvfile:
+        filewriter = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        for x in range(num_new_) :
+            filewriter.writerow([SEList[num_loaded + x].speed, SEList[num_loaded + x].efficiency])
+#pass a SpeedandEfficiency object
+#have to manually update MaxSE this only returns the new max
+#is this how python for loops work?
+def UpdateEMax(currentMax):
+    for ele in SEList :
+        if ele.efficiency < currentMax.efficiency :
+            currentMax = SEList[i]
+    return currentMax
+def speedVector():
+    speedvector = []
+    i = 0
+    while i < len(SEList) :
+       speedvector.append(float(SEList[i].speed))
+       i += 1
+    return speedvector
+def effVector():
+    effvector = []
+    i = 0
+    while i < len(SEList) :
+        effvector.append(float(SEList[i].efficiency))
+        i += 1
+    return effvector
+#current list format
+#SPEED,EFFICIENCY //Format marker
+#CURRENT,MAX // max efficiency point
+#DATA,POINTS // list of all efficiency points
--- obd_hud.py
+++ obd_hud.py
@@ -1,18 +1,21 @@
-from Tkinter import *
+#import matplotlib.pyplot as plt
+import Tkinter as tk
 import ttk
-# import matplotlib
-# matplotlib.use("TkAgg")
-# from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2TkAgg
-# from matplotlib.figure import Figure
-# import matplotlib.pyplot
-
 
 class OBDCluster:
 
 	def __init__(self, master):
-		speed = 85;
-		rpm = 3200;
-		efficient_speed = 54;
+		speed = 85
+		rpm = 3200
+		efficient_speed = 54
+
+		# speed_vector = [5,10,15,20,25,30,35,40,45,50,55,60,65,70,75]
+		# eff_vector = [12,14,20.3,26.2,27.3,28.7,29.4,30.1,30.9,31.3,32.1,29.9,26.6,24.8,23.5]
+
+		# plt.plot(speed_vector, eff_vector)
+		# plt.xlabel("speed")
+		# plt.ylabel("mpg")
+		# plt.savefig("figureOBD.png", format="png")
 
 		root.update()
 		screen_width = master.winfo_screenwidth()
@@ -23,95 +26,81 @@ class OBDCluster:
 
 		root.bind("<Escape>", close)
 		
-		leftframe = Frame(master)
-		leftframe.pack(side=LEFT)
+		leftframe = tk.Frame(master)
+		leftframe.pack(side=tk.LEFT)
 		leftframe.configure(background="#d0d0d4")
 
-		topleftframe = Frame(leftframe)
-		topleftframe.pack(side=TOP, padx=.05*screen_width, pady=0.1*screen_height)
+		topleftframe = tk.Frame(leftframe)
+		topleftframe.pack(side=tk.TOP, padx=.05*screen_width, pady=0.1*screen_height)
 		topleftframe.configure(background="#d0d0d4")
 
-		bottomleftframe = Frame(leftframe)
-		bottomleftframe.pack(side=BOTTOM, padx=.05*screen_width, pady=.1*screen_height)
+		bottomleftframe = tk.Frame(leftframe)
+		bottomleftframe.pack(side=tk.BOTTOM, padx=.05*screen_width, pady=.1*screen_height)
 		bottomleftframe.configure(background="#d0d0d4")
 
 		self.speedLabel = ttk.Label(topleftframe, text=speed, font="Arial 90", background="#d0d0d4")
-		self.speedLabel.pack(side=TOP)
+		self.speedLabel.pack(side=tk.TOP)
 
 		self.speedUnitsLabel = ttk.Label(topleftframe, text="MPH", font="Arial 15", background="#d0d0d4")
-		self.speedUnitsLabel.pack(side=BOTTOM)
+		self.speedUnitsLabel.pack(side=tk.BOTTOM)
 
 		self.RPMLabel = ttk.Label(bottomleftframe, text=rpm, font="Arial 50", background="#d0d0d4")
-		self.RPMLabel.pack(side=TOP)
+		self.RPMLabel.pack(side=tk.TOP)
 
 		self.RPMUnitsLabel = ttk.Label(bottomleftframe, text="RPM", font="Arial 15", background="#d0d0d4")
-		self.RPMUnitsLabel.pack(side=BOTTOM)
+		self.RPMUnitsLabel.pack(side=tk.BOTTOM)
 
-		midframe = Frame(master)
-		midframe.pack(side=LEFT)
+		midframe = tk.Frame(master)
+		midframe.pack(side=tk.LEFT)
 		midframe.configure(background="#d0d0d4")
 
-		topmidframe = Frame(midframe)
-		topmidframe.pack(side=TOP, padx=0.1*screen_width, pady=.1*screen_height)
+		topmidframe = tk.Frame(midframe)
+		topmidframe.pack(side=tk.TOP, padx=0.1*screen_width, pady=.1*screen_height)
 		topmidframe.configure(background="#d0d0d4")
 
-		bottommidframe = Frame(midframe)
-		bottommidframe.pack(side=BOTTOM, padx=0.1*screen_width, pady=.1*screen_height)
+		bottommidframe = tk.Frame(midframe)
+		bottommidframe.pack(side=tk.BOTTOM, padx=0.1*screen_width, pady=.1*screen_height)
 		bottommidframe.configure(background="#d0d0d4")
 
 		self.titleLabel = ttk.Label(topmidframe, text="Fuel Economy", font="Arial 40", background="#d0d0d4")
-		self.titleLabel.pack(side=TOP)
+		self.titleLabel.pack(side=tk.TOP)
 
-		# self.canvas = Canvas(bottommidframe, width=430, height=400)
-		# self.canvas.pack()
-
-		# f = pyplot(figsize=(5,5), dpi=100)
-		# f.plot([1,2,3],[1,3,5])
-		# f.xlabel("sp")
-		# f.ylabel("mpg")
-        # a = f.add_subplot(111)
-        # a.plot([1,2,3,4,5,6,7,8],[5,6,1,3,8,9,3,5])
-
-        # self.canvas = FigureCanvasTkAgg(f, bottommidframe)
-        # self.canvas.show()
-        # self.canvas.get_tk_widget().pack(side=BOTTOM, fill=BOTH, expand=True)
-
-        # toolbar = NavigationToolbar2TkAgg(canvas, self)
-        # toolbar.update()
-        # self.canvas._tkcanvas.pack(side=TOP, fill=BOTH, expand=True)
+		# self.img = ImageTk.PhotoImage(Image.open("figureOBD.png"))
+		# self.panel = tk.Label(root, image = img)
+		# self.panel.pack(side = "bottom", fill = "both", expand = "yes")
 		
 		def recordData():
 			print("button clicked..")
 
-		rightframe = Frame(master)
-		rightframe.pack(expand=YES, fill=BOTH)
+		rightframe = tk.Frame(master)
+		rightframe.pack(expand=tk.YES, fill=tk.BOTH)
 		rightframe.configure(background="#dee0e4")
 
-		toprightframe = Frame(rightframe)
-		toprightframe.pack(side=TOP, padx=6, pady=65)
+		toprightframe = tk.Frame(rightframe)
+		toprightframe.pack(side=tk.TOP, padx=6, pady=65)
 		toprightframe.configure(background="#dee0e4")
 
-		bottomrightframe = Frame(rightframe)
-		bottomrightframe.pack(side=BOTTOM, padx=.04*screen_width, pady=.1*screen_height)
+		bottomrightframe = tk.Frame(rightframe)
+		bottomrightframe.pack(side=tk.BOTTOM, padx=.04*screen_width, pady=.1*screen_height)
 		bottomrightframe.configure(background="#dee0e4")
 
-		self.message = Message(toprightframe, text="Capture fuel consuption data at a constant speed", font="Arial 13", background="#dee0e4", width="300")
-		self.message.pack(side=TOP, padx=5, pady=25)
+		self.message = tk.Message(toprightframe, text="Capture fuel consuption data at a constant speed", font="Arial 13", background="#dee0e4", width="300")
+		self.message.pack(side=tk.TOP, padx=5, pady=25)
 		
 		self.recordButton = ttk.Button(toprightframe, text="Cruise", command=recordData())
-		self.recordButton.pack(side=BOTTOM)
+		self.recordButton.pack(side=tk.BOTTOM)
 
 		self.efficiencyLabel = ttk.Label(bottomrightframe, text="Most Fuel Efficient Speed", font="Arial 13", background="#dee0e4")
-		self.efficiencyLabel.pack(side=TOP)
+		self.efficiencyLabel.pack(side=tk.TOP)
 
 		self.bestSpeedLabel = ttk.Label(bottomrightframe, text=efficient_speed, font="Arial 60", background="#dee0e4")
-		self.bestSpeedLabel.pack(side=TOP)
+		self.bestSpeedLabel.pack(side=tk.TOP)
 
 		self.bestSpeedUnitsLabel = ttk.Label(bottomrightframe, text="MPH", font="Arial 15", background="#dee0e4")
-		self.bestSpeedUnitsLabel.pack(side=BOTTOM)
+		self.bestSpeedUnitsLabel.pack(side=tk.BOTTOM)
 
 
-root = Tk()
+root = tk.Tk()
 obd = OBDCluster(root)
 root.configure(background="#d0d0d4")
 root.attributes('-fullscreen', True)
